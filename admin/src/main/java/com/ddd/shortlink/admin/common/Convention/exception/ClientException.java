cb2deac9528f416c931f03ff46c82b1fbc586673
mumu8 <834524806@qq.com>
2024-02-15 16:57:49 +0800

    feature: DIY three kind of exception

---
+++ admin/src/main/java/com/ddd/shortlink/admin/common/Convention/exception/ClientException.java
@@ -0,0 +1,39 @@
+package com.ddd.shortlink.admin.common.Convention.exception;
+
+/**
+ * @param
+ * @return
+ **/
+
+import com.ddd.shortlink.admin.common.Convention.errorcode.BaseErrorCode;
+import com.ddd.shortlink.admin.common.Convention.errorcode.IErrorCode;
+
+/**
+ * 客户端异常
+ */
+public class ClientException extends AbstractException {
+
+    public ClientException(IErrorCode errorCode) {
+        this(null, null, errorCode);
+    }
+
+    public ClientException(String message) {
+        this(message, null, BaseErrorCode.CLIENT_ERROR);
+    }
+
+    public ClientException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public ClientException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "ClientException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
