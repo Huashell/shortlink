f56ea28c44416a66307960a54c2cdfc9fde4e845
mumu8 <834524806@qq.com>
2024-02-16 17:37:34 +0800

    feature: admin目录下的控制器uri中加上admin

--- admin/src/main/java/com/ddd/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/ddd/shortlink/admin/controller/UserController.java
@@ -27,12 +27,12 @@ public class UserController {
     /**
     * @Description:根据用户名查询用户信息
     **/
-    @GetMapping("/api/short-link/v1/user/{username}")
+    @GetMapping("/api/short-link/admin/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
         return Results.success(userService.getUserByUsername(username));
     }
 
-    @GetMapping("/api/short-link/v1/actual/user/{username}")
+    @GetMapping("/api/short-link/admin/v1/actual/user/{username}")
     public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username){
         return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
@@ -40,7 +40,7 @@ public class UserController {
     /**
     * 查询用户名是否可用
     **/
-    @GetMapping("/api/short-link/v1/user/has-username")
+    @GetMapping("/api/short-link/admin/v1/user/has-username")
     public Result<Boolean> hasUsername(@RequestParam("username") String username){
         return Results.success(!userService.hasUsername(username));
     }
@@ -49,7 +49,7 @@ public class UserController {
      * 用户注册
      */
 
-    @PostMapping("/api/short-link/v1/user")
+    @PostMapping("/api/short-link/admin/v1/user")
     public Result<Void> register(@RequestBody UserRegisterReqDTO registerReqDTO){
         userService.register(registerReqDTO);
         return Results.success();
@@ -57,7 +57,7 @@ public class UserController {
     /**
     * 用户更新数据
     **/
-    @PutMapping("/api/short-link/v1/user")
+    @PutMapping("/api/short-link/admin/v1/user")
     public Result<Void> update(@RequestBody UserUpdateReqDTO reqDTO){
         userService.update(reqDTO);
         return Results.success();
@@ -66,18 +66,18 @@ public class UserController {
      *
      */
 
-    @PostMapping("/api/short-link/v1/user/login")
+    @PostMapping("/api/short-link/admin/v1/user/login")
     public Result<UserLoginRespDTO> login(@RequestBody UserLoginReqDTO userLoginReqDTO){
         return Results.success(userService.login(userLoginReqDTO));
 
     }
 
-    @GetMapping("/api/short-link/v1/user/check-login")
+    @GetMapping("/api/short-link/admin/v1/user/check-login")
     public Result<Boolean> checkLogin(@RequestParam("username") String username, @RequestParam("token") String token){
         return Results.success(userService.checkLogin(username, token));
     }
 
-    @DeleteMapping("/api/short-link/v1/user/logout")
+    @DeleteMapping("/api/short-link/admin/v1/user/logout")
     public Result<Void> logout(@RequestParam("username") String username, @RequestParam("token") String token){
         userService.logout(username, token);
         return Results.success();
