f15a4fed81cd8e1a41146459cc06f848d083f401
mumu8 <834524806@qq.com>
2024-02-17 20:53:26 +0800

    feature: 注册用户后创建默认分组

--- admin/src/main/java/com/ddd/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/ddd/shortlink/admin/service/impl/UserServiceImpl.java
@@ -15,6 +15,7 @@ import com.ddd.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.ddd.shortlink.admin.dto.req.UserUpdateReqDTO;
 import com.ddd.shortlink.admin.dto.resp.UserLoginRespDTO;
 import com.ddd.shortlink.admin.dto.resp.UserRespDTO;
+import com.ddd.shortlink.admin.service.GroupService;
 import com.ddd.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
@@ -42,6 +43,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
    private final RedissonClient redissonClient;
    private final StringRedisTemplate stringRedisTemplate;
+   private final GroupService groupService;
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
@@ -77,13 +79,13 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
                     throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
                 }
                 userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                groupService.saveGroup("默认分组");
                 return;
             }
             throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
         } finally {
             lock.unlock();
         }
-
     }
 
     @Override
