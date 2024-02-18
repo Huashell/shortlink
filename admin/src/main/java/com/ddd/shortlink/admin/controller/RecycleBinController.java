a936f05f3be56bb3ffd872ef02ad2f6fd5f72aa9
mumu8 <834524806@qq.com>
2024-02-18 21:30:03 +0800

    feature: 开发短链接回收站功能

---
+++ admin/src/main/java/com/ddd/shortlink/admin/controller/RecycleBinController.java
@@ -0,0 +1,68 @@
+package com.ddd.shortlink.admin.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.ddd.shortlink.admin.common.Convention.result.Result;
+import com.ddd.shortlink.admin.common.Convention.result.Results;
+import com.ddd.shortlink.admin.remote.ShortLinkRemoteService;
+import com.ddd.shortlink.admin.remote.dto.req.RecycleBinRecoverReqDTO;
+import com.ddd.shortlink.admin.remote.dto.req.RecycleBinRemoveReqDTO;
+import com.ddd.shortlink.admin.remote.dto.req.RecycleBinSaveReqDTO;
+import com.ddd.shortlink.admin.remote.dto.req.ShortLinkRecycleBinPageReqDTO;
+import com.ddd.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import com.ddd.shortlink.admin.service.RecycleBinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 回收站管理控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class RecycleBinController {
+
+    private final RecycleBinService recycleBinService;
+
+    /**
+     * 后续重构为 SpringCloud Feign 调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+
+    /**
+     * 保存回收站
+     */
+    @PostMapping("/api/short-link/admin/v1/recycle-bin/save")
+    public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
+        shortLinkRemoteService.saveRecycleBin(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 分页查询回收站短链接
+     */
+    @GetMapping("/api/short-link/admin/v1/recycle-bin/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkRecycleBinPageReqDTO requestParam) {
+        return recycleBinService.pageRecycleBinShortLink(requestParam);
+    }
+
+    /**
+     * 恢复短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/recycle-bin/recover")
+    public Result<Void> recoverRecycleBin(@RequestBody RecycleBinRecoverReqDTO requestParam) {
+        shortLinkRemoteService.recoverRecycleBin(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 移除短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/recycle-bin/remove")
+    public Result<Void> removeRecycleBin(@RequestBody RecycleBinRemoveReqDTO requestParam) {
+        shortLinkRemoteService.removeRecycleBin(requestParam);
+        return Results.success();
+    }
+}
---
+++ admin/src/main/java/com/ddd/shortlink/admin/service/RecycleBinService.java
@@ -0,0 +1,24 @@
+package com.ddd.shortlink.admin.service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.ddd.shortlink.admin.common.Convention.result.Result;
+import com.ddd.shortlink.admin.remote.dto.req.ShortLinkRecycleBinPageReqDTO;
+import com.ddd.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+
+
+/**
+ * URL 回收站接口层
+ */
+public interface RecycleBinService {
+
+    /**
+     * 分页查询回收站短链接
+     *
+     * @param requestParam 请求参数
+     * @return 返回参数包装
+     */
+    Result<IPage<ShortLinkPageRespDTO>> pageRecycleBinShortLink(ShortLinkRecycleBinPageReqDTO requestParam);
+
+
+
+}
